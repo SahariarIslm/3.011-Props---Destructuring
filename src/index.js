da8c7618e726eeec64322ffc2291c6618c1e62d7
Sahariar Islam <dhruv.islam7584@gmail.com>
2022-05-17 18:15:52 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+// CSS
+import './index.css';
+// setup vars
+const firstBook = {
+    title:'A Letter From Your Teacher: On the Last Day',
+    author:'Shannon Olsen',
+    img:"https://images-na.ssl-images-amazon.com/images/I/61AqjgMxpLL._AC_UL604_SR604,400_.jpg"
+};
+const secondBook = {
+    title:'I Love You to the Moon and Back',
+    author:'Amelia Hepworth',
+    img:"https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg"
+};
+function BookList(){
+    return (
+        <section className='booklist'> 
+            <Book 
+                img={firstBook.img} 
+                title={firstBook.title} 
+                author={firstBook.author} 
+            />
+            <Book 
+                img={secondBook.img} 
+                title={secondBook.title} 
+                author={secondBook.author}  
+            />
+        </section>
+    );
+}
+
+const Book = ({img,title,author}) =>{
+    // const {img,title,author} = property
+    return <article className='book'>
+        <img src={img} alt=""/>
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+    </article>
+};
+
+ReactDOM.render(<BookList/>,document.getElementById('root'));
